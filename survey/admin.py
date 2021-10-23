e9660df889f1ddf83fd423cce6add2d0db74b1f2
spacekay <bethjkim0311@gmail.com>
2021-10-23 21:42:13 +0900

    edited admin models

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+from .models import Choice
+from .models import Song
+from .models import Result
+
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Song)
+admin.site.register(Result)
