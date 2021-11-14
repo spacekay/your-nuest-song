c17e4cbc25b1c14adf1d71d7df159e269fb0e4fd
spacekay <bethjkim0311@gmail.com>
2021-11-15 00:55:01 +0900

    deleted some lines not using

--- survey/urls.py
+++ survey/urls.py
@@ -5,10 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('questions/<qid>', views.questions, name='questions'),
-    path('select', views.select, name='select'),
     path('result', views.result, name='result'),
     path('like', views.like, name='like'),
     path('dislike', views.dislike, name='dislike'),
-    #path('<str:param>/', views.get_parameter),
     path('choice', views.choice, name='choice')
 ]
