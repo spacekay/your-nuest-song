700adb7b039f9bd3233ee18616e75900703b5560
spacekay <bethjkim0311@gmail.com>
2021-12-02 19:22:33 +0900

    edited url pattern

--- survey/urls.py
+++ survey/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('questions/<qid>', views.questions, name='questions'),
     path('result', views.result, name='result'),
-    path('like', views.like, name='like'),
-    path('dislike', views.dislike, name='dislike'),
+    path('like/<rid>', views.like, name='like'),
+    path('dislike/<rid>', views.dislike, name='dislike'),
     path('choice', views.choice, name='choice')
 ]
